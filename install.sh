c362e571e9675be52e6b2e5cdf34a25ed4e6d50c
vilukissa <vaino.granat1@gmail.com>
2019-02-13 21:12:05 +0200

    Added auto vundle install

--- install.sh
+++ install.sh
@@ -2,6 +2,15 @@
 
 dotfiles_dir=~/dotfiles
 
+while true; do
+    read -p "Do you want to set up vundle? y/n " yn
+    case $yn in
+        [Yy]* ) git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
 sudo rm -rf ~/.vim > /dev/null 2>&1
 sudo rm -rf ~/.vimrc > /dev/null 2>&1
 sudo rm -rf ~/.tmux.conf > /dev/null 2>&1

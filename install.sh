911cef8c0e3ccea2cec68fe5aefcbb2723f1210b
vilukissa <vaino.granat1@gmail.com>
2019-02-13 20:52:10 +0200

    Added lock icon to install script

--- install.sh
+++ install.sh
@@ -8,11 +8,14 @@ sudo rm -rf ~/.tmux.conf > /dev/null 2>&1
 sudo rm -rf ~/.zshrc > /dev/null 2>&1
 sudo rm -rf ~/.config > /dev/null 2>&1
 sudo rm -rf ~/.Xdefaults > /dev/null 2>&1
+sudo rm -r ~/lock.png > /dev/null 2>&1
 
 cp $dotfiles_dir/.vimrc ~
 cp $dotfiles_dir/.tmux.conf ~
 cp $dotfiles_dir/.zshrc ~
 cp $dotfiles_dir/.Xdefaults ~
+cp $dotfiles_dir/lock.png ~
+
 cp -r $dotfiles_dir/.vim ~
 cp -r $dotfiles_dir/.config ~
 

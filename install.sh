d80b4c82f7372f27b7931a420d18f6ef03915f8a
vilukissa <vaino.granat1@gmail.com>
2019-02-13 21:23:05 +0200

    Toned down install script

--- install.sh
+++ install.sh
@@ -11,21 +11,13 @@ while true; do
     esac
 done
 
-sudo rm -rf ~/.vim > /dev/null 2>&1
-sudo rm -rf ~/.vimrc > /dev/null 2>&1
-sudo rm -rf ~/.tmux.conf > /dev/null 2>&1
-sudo rm -rf ~/.zshrc > /dev/null 2>&1
-sudo rm -rf ~/.config > /dev/null 2>&1
-sudo rm -rf ~/.Xdefaults > /dev/null 2>&1
-sudo rm -r ~/lock.png > /dev/null 2>&1
-
 cp $dotfiles_dir/.vimrc ~
 cp $dotfiles_dir/.tmux.conf ~
 cp $dotfiles_dir/.zshrc ~
 cp $dotfiles_dir/.Xdefaults ~
 cp $dotfiles_dir/lock.png ~
 
-cp -r $dotfiles_dir/.vim ~
+cp -r $dotfiles_dir/.vim/ftplugin ~/.vim/
 cp -r $dotfiles_dir/.config ~
 
 echo "Install finished"

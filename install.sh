8f94a5b63898355c190f882cf9642baae10a9132
vilukissa <vaino.granat1@gmail.com>
2019-02-13 20:49:33 +0200

    Added install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+# Symlink.sh
+
+dotfiles_dir=~/dotfiles
+
+sudo rm -rf ~/.vim > /dev/null 2>&1
+sudo rm -rf ~/.vimrc > /dev/null 2>&1
+sudo rm -rf ~/.tmux.conf > /dev/null 2>&1
+sudo rm -rf ~/.zshrc > /dev/null 2>&1
+sudo rm -rf ~/.config > /dev/null 2>&1
+sudo rm -rf ~/.Xdefaults > /dev/null 2>&1
+
+cp $dotfiles_dir/.vimrc ~
+cp $dotfiles_dir/.tmux.conf ~
+cp $dotfiles_dir/.zshrc ~
+cp $dotfiles_dir/.Xdefaults ~
+cp -r $dotfiles_dir/.vim ~
+cp -r $dotfiles_dir/.config ~
+
+echo "Install finished"

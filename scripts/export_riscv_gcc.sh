4e37c57ed4e9666a30a4d618ddea1b3e120d3b0f
Väinö Granat <vaino-waltteri.granat@tuni.fi>
2024-05-17 15:00:25 +0300

    Work scripts

---
+++ scripts/export_riscv_gcc.sh
@@ -0,0 +1,4 @@
+export CC=/opt/homebrew/Cellar/riscv-gnu-toolchain/main/bin/riscv64-unknown-elf-gcc
+export CXX=/opt/homebrew/Cellar/riscv-gnu-toolchain/main/bin/riscv64-unknown-elf-g++
+export OBJDUMP=/opt/homebrew/Cellar/riscv-gnu-toolchain/main/bin/riscv64-unknown-elf-objdump
+export PATH="/opt/homebrew/Cellar/riscv-gnu-toolchain/main/bin:$PATH"
---
+++ scripts/include_tvm.sh
@@ -0,0 +1,3 @@
+export TVM_HOME=$HOME/work/tvm
+export TVM_LIBRARY_PATH=$HOME/work/tvm/build
+export PYTHONPATH=$TVM_HOME/python:${PYTHONPATH}

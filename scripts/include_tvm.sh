fee5964069a23377a4c0fba477236035c30e4524
Väinö Granat <vaino-waltteri.granat@tuni.fi>
2025-04-27 17:12:49 +0300

    Update include tvm script

--- scripts/include_tvm.sh
+++ scripts/include_tvm.sh
@@ -1,3 +1,3 @@
 export TVM_HOME=$HOME/work/tvm
-export TVM_LIBRARY_PATH=$HOME/work/tvm/build
+export TVM_BUILD_PATH=/Users/vainogranat/work/tvm/build
 export PYTHONPATH=$TVM_HOME/python:${PYTHONPATH}

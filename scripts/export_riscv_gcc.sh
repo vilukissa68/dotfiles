4b96c3b35cc357812396fea213c3891ec22af37b
Väinö Granat <vaino-waltteri.granat@tuni.fi>
2025-04-27 17:13:04 +0300

    Update export riscv script

--- scripts/export_riscv_gcc.sh
+++ scripts/export_riscv_gcc.sh
@@ -2,3 +2,8 @@ export CC=/opt/homebrew/Cellar/riscv-gnu-toolchain/main/bin/riscv64-unknown-elf-
 export CXX=/opt/homebrew/Cellar/riscv-gnu-toolchain/main/bin/riscv64-unknown-elf-g++
 export OBJDUMP=/opt/homebrew/Cellar/riscv-gnu-toolchain/main/bin/riscv64-unknown-elf-objdump
 export PATH="/opt/homebrew/Cellar/riscv-gnu-toolchain/main/bin:$PATH"
+
+# Gitlab riscv-gcc
+export RISCV_DIR_GCC=/opt/homebrew/Cellar/riscv-gnu-toolchain/main/
+alias riscv32-unknown-elf-gcc=riscv64-unknown-elf-gcc
+
